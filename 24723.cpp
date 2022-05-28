6e1c12545b5a87d6d22392efd0fe0b2f40ff20c2
engallote <53823323+engallote@users.noreply.github.com>
2022-05-29 00:07:06 +0900

    Add files via upload

---
+++ 24723.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(void) {
+    int N;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    cout << pow(2, N);
+
+    return 0;
+}
