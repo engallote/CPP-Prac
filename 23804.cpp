bdfab6b3a4d5fdd2b9ead5b46ceab6448eb04bc5
engallote <53823323+engallote@users.noreply.github.com>
2024-02-18 04:51:32 +0900

    Add files via upload

---
+++ 23804.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <bitset>
+#include <cstdlib>
+#include <sstream>
+#include <regex>
+using namespace std;
+
+int main(void)
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cout << "@@@@@";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < N * 3; i++) {
+        for(int j = 0; j < N; j++) {
+            cout << "@";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cout << "@@@@@";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
